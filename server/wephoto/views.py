0446dc8652caaa43a1bcf1f9b9b87332e27a9bbd
Your Name <you@example.com>
2018-10-21 07:35:19 +0800

    upload image filename

--- server/wephoto/views.py
+++ server/wephoto/views.py
@@ -75,7 +75,8 @@ def upload_image(req):
         print(req.FILES)
         if not myFile:
             return HttpResponse("no files for upload!")
-        destination = open(os.path.join(settings.MEDIA_ROOT,myFile.name),'wb+')    # 打开特定的文件进行二进制的写操作
+
+        destination = open(os.path.join(settings.MEDIA_ROOT,str(time.time())+myFile.name),'wb+')    # 打开特定的文件进行二进制的写操作
         for chunk in myFile.chunks():      # 分块写入文件
             destination.write(chunk)
         destination.close()
